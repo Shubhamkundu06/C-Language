45bee216e26ee854167a2b023415e03b285da072
Shubhamkundu06 <shubhamkundu83@gmail.com>
2024-08-18 10:11:22 +0530

    for loop question 1

---
+++ 3-loops/question.c
@@ -0,0 +1,13 @@
+// Print hello world 'n' times. Take 'n' as input from user
+#include <stdio.h>
+int main()
+{
+    int n; 
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++)
+    {
+        printf("hellow world\n");
+    }
+    return 0;
+}
