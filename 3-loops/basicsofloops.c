dbd8f95b8a960d334588fd8903644887d8fc3e0b
Shubhamkundu06 <shubhamkundu83@gmail.com>
2024-08-18 10:05:54 +0530

    basic of for loop

---
+++ 3-loops/basicsofloops.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    //i++ is equal to i=i+1 dono ka matlab same hote hai
+    // int n; 
+    // printf("Enter a number : ");
+    // scanf("%d", &n);
+    for (int i=1; i<=10; i=i+3)
+    {
+        printf("hellow world\n");
+    }
+    return 0;
+}
