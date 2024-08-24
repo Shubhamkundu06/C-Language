9ba1ade3ddcfd11f8bacb2b985ff3ac2baf04129
Shubhamkundu06 <shubhamkundu83@gmail.com>
2024-08-24 17:49:53 +0530

    while using for loop

--- 3-loops/continue.c
+++ 3-loops/continue.c
@@ -1,13 +1,14 @@
 // For loop with continue statment.
+
 // WAP to print odd numbers from 1 to 100.
 
 #include <stdio.h>
 int main()
 {
-
+ 
     for (int i = 1; i <= 100; i++)
     {
-        if (i % 2 == 0)
+        if (i % 2 == 0) //even
         {
             continue;
         }
