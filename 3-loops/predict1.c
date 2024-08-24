b9b6c98ed823febc04325b67fa2316eaba3240be
Shubhamkundu06 <shubhamkundu83@gmail.com>
2024-08-24 20:38:07 +0530

    while loop home work 2

--- 3-loops/predict1.c
+++ 3-loops/predict1.c
@@ -3,11 +3,12 @@
 #include <stdio.h>
 int main()
 {
-    int i = 1;
-    while (i <= 10) 
+    int x = 1;
+    while (x==1) 
     {
-        printf("%d ", i);
-        i++;
+        x = x-1;
+        printf("\n%d ", x);
+        
     }
 
     return 0;
