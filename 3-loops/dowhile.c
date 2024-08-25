dfda91fe8e975bd3f63a83f4fb45aab9df7214ed
Shubhamkundu06 <shubhamkundu83@gmail.com>
2024-08-25 20:03:51 +0530

    dowhile using for loop

---
+++ 3-loops/dowhile.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i = 10;
+    // while(i<10){
+    //     printf("hello");
+    //     i++;
+    // }
+    do{
+        printf("hello\n");
+        i++;
+    } while(i<10);
+
+
+    return 0;
+}
