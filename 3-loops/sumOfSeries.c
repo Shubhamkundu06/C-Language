32cbbf8cad85acb9c7cac752cc8b75de6ceffff0
Shubhamkundu06 <shubhamkundu83@gmail.com>
2024-09-01 20:20:40 +0530

    sumOfSeries using another method

--- 3-loops/sumOfSeries.c
+++ 3-loops/sumOfSeries.c
@@ -1,6 +1,6 @@
- /* Question : Print the sum of this series :
+/* Question : Print the sum of this series :
   1 - 2 + 3 - 4 + 5 - 6... upto'n'. */
-    //  Method number : 1
+    //  Method number : 2
 
 #include<stdio.h>
 int main(){
@@ -9,10 +9,13 @@ int main(){
    scanf("%d",&n);
     // 1 - 2 + 3 - 4 + 5 - 6 .... n terms 
     int sum = 0;
-    for(int i=1;i<=n;i++){
-    if(i%2!=0) sum = sum + i;
-    else sum = sum - i;
-}
+    if(n%2==0){
+        sum = -n/2;
+    }
+    else{
+        sum = -n/2 + n;
+    }
+
 printf("The sum is : %d",sum);
     return 0;
 }
