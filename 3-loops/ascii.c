fa91b5f653869dd1930532b3df7fa7a1b8b59a1b
Shubhamkundu06 <shubhamkundu83@gmail.com>
2024-09-02 18:01:31 +0530

    ascii using for loop

---
+++ 3-loops/ascii.c
@@ -0,0 +1,15 @@
+/* WAP to print all the ASCII values and their equivalent 
+characters of 26 alphabets using a while loop. */
+
+#include<stdio.h>
+int main(){
+    // what is typecasting
+    for(int i=65;i<=90;i++){
+        printf("%d ",i);
+        char ch = (char)i;
+        printf("%c\n",ch);
+
+    }
+    return 0;
+}
+
