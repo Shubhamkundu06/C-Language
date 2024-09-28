a742c51285da8059ced23dac5d9005d5a7856e47
Shubhamkundu06 <shubhamkundu83@gmail.com>
2024-09-28 21:18:21 +0530

    basic using for function

---
+++ 5-functions/sqrt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+
+    // sqrt meaning => square root 
+    int root = sqrt(a); 
+    printf("The square root is : %d",root);
+   
+    // pow meaning => power
+    // int q = pow(2,6); // number change and the final answer
+    // printf("%d",q);
+    //  return 0;
+}
